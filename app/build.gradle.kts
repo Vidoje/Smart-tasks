b9ae958b60750a0b59261bc1b8a81b136fd8ab8d
Vidoje Muric <vidojemuric@gmail.com>
2025-10-23 18:38:45 +0200

    Added libraries and assets

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,17 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.org.jetbrains.kotlin.kapt)
 }
 
 android {
     namespace = "com.taurunium.smarttasks"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.taurunium.smarttasks"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -27,22 +28,45 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
+    }
+
+    buildFeatures {
+        dataBinding = true
     }
 }
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
+    // Core UI
+    implementation(libs.bundles.androidx.core.ui)
+
+    // Lifecycle (ViewModel, LiveData, SavedState)
+    implementation(libs.bundles.lifecycle)
+    kapt(libs.androidx.lifecycle.compiler)
+
+    // Coroutines
+    implementation(libs.kotlinx.coroutines.android)
+
+    // Dagger (DI)
+    implementation(libs.dagger)
+    kapt(libs.dagger.compiler)
+
+    // Networking
+    implementation(libs.bundles.network)
+
+    // Images
+    implementation(libs.glide)
+    kapt(libs.glide.compiler)
+
+    // Tests
+    testImplementation(libs.junit4)
     androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.espresso.core)
+
+
 }
